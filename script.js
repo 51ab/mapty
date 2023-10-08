e7f6352019be7c2cda252f3feacc117209fe0a5f
Akshi <bhattakshi3@gmail.com>
2023-10-08 17:33:40 +0530

    navigator added

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map;
+let map, mapEvent;
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
@@ -27,25 +27,10 @@ if (navigator.geolocation) {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
-      map.on('click', function (mapEvent) {
+      map.on('click', function (mapE) {
+        mapEvent = mapE;
         form.classList.remove('hidden');
         inputDistance.focus();
-
-        /* console.log(mapEvent);
-            const { lat, lng } = mapEvent.latlng;
-            L.marker([lat, lng])
-            .addTo(map)
-            .bindPopup(
-                L.popup({
-                maxWidth: 250,
-                minWidth: 100,
-                autoClose: false,
-                closeOnClick: false,
-                className: 'running-popup',
-                })
-            )
-            .setPopupContent('Workout')
-            .openPopup(); */
       });
     },
     function () {
@@ -54,7 +39,14 @@ if (navigator.geolocation) {
   );
 }
 
-form.addEventListener('submit', function () {
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  inputDistance.value =
+    inputCadence.value =
+    inputDuration.value =
+    inputElevation =
+      '';
+
   console.log(mapEvent);
   const { lat, lng } = mapEvent.latlng;
   L.marker([lat, lng])
