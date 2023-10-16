0a2c18b54ea99bea7db6728e6bdecf27d4b2671e
Akshi <bhattakshi3@gmail.com>
2023-10-16 17:43:49 +0530

    new changes

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {}
+
 let map, mapEvent;
 
 class App {
