06d609d44c26c7c96bcf7d8da194d9ce9440b541
Akshi <bhattakshi3@gmail.com>
2023-10-17 12:11:35 +0530

    new changes

--- script.js
+++ script.js
@@ -37,6 +37,10 @@ class Cycling extends Workout {
   constructor(coords, distance, duration, elevationGain) {
     super(coords, distance, duration);
     this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+  calcSpeed() {
+    this.speed = this.distance / this.duration;
   }
 }
 
