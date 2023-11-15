c0c26c9bd86b0cfb21b9bffd799987601c36ab57
Akshi <bhattakshi3@gmail.com>
2023-11-15 15:10:20 +0530

    new changes

--- script.js
+++ script.js
@@ -9,10 +9,12 @@ class Workout {
     this.coords = coords;
     this.distance = distance;
     this.duration = duration;
+    this._setDescription();
   }
-  setDescription(){
+  _setDescription(){
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    this.description
+    this.description=`${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]}} 
+    ${this.date.getDate()}`;
   }
 }
 
